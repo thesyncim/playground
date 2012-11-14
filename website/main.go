db3da5fe10ada1df1e3ec4ae83696948ef67b31f
syncim <thesyncim@gmail.com>
2012-11-14 00:53:51 +0000

    mvc test

--- website/main.go
+++ website/main.go
@@ -10,13 +10,13 @@ package website
 
 import ("github.com/gorilla/mux"
 	"net/http"
-	"github.com/thesyncim/playground/website/controllers"
+
 
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/{teste}/{id}", controllers.HomeHandler)
+//	r.HandleFunc("/{teste}/{id}", controllers.HomeHandler)
 	http.Handle("/", r)
 	http.ListenAndServe(":8090", r)
 
